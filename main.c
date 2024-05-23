2bf7bbb3e2c58d524205178f35504353b77e09e9
Anton Kolesov <kolesov.anton.s@gmail.com>
2024-05-24 01:47:44 +0300

    Added run script, modified main.c example

--- main.c
+++ main.c
@@ -1,19 +1,35 @@
 #include "third_party/mongoose.h"
+#include <stdlib.h>
+
+static void runCode(char *code) {
+  printf("hello world\n");
+}
 
 // Connection event handler function
 static void fn(struct mg_connection *c, int ev, void *ev_data) {
-    if (ev == MG_EV_HTTP_MSG) {  // New HTTP request received
-        struct mg_http_message *hm = (struct mg_http_message *) ev_data;  // Parsed HTTP request
-        mg_http_reply(c, 200, "", "{%m:%s}\n", MG_ESC("message"), "hello world");    // Yes. Respond JSON
-    }
+  if (ev == MG_EV_HTTP_MSG) { // New HTTP request received
+    struct mg_http_message *hm =
+        (struct mg_http_message *)ev_data; // Parsed HTTP request
+    runCode("hello world");
+    mg_http_reply(c, 200, "", "{%m:%s}\n", MG_ESC("message"),
+                  "hello world"); // Yes. Respond JSON
+  }
+}
+
+static void setup_output_buffer() { setvbuf(stdout, NULL, _IONBF, -1); }
+
+static void start_server() {
+  struct mg_mgr mgr; // Mongoose event manager. Holds all connections
+
+  mg_mgr_init(&mgr); // Initialise event manager
+  mg_http_listen(&mgr, "http://0.0.0.0:8000", fn, NULL); // Setup listener
+  for (;;) {
+    mg_mgr_poll(&mgr, 999); // Infinite event loop
+  }
 }
 
 int main() {
-    struct mg_mgr mgr;  // Mongoose event manager. Holds all connections
-    mg_mgr_init(&mgr);  // Initialise event manager
-    mg_http_listen(&mgr, "http://0.0.0.0:8000", fn, NULL);  // Setup listener
-    for (;;) {
-        mg_mgr_poll(&mgr, 1000);  // Infinite event loop
-    }
-    return 0;
+  setup_output_buffer();
+  start_server();
+  return 0;
 }
