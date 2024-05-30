88a02531f21a68f2ad60fffb03cfe5de4fc7d276
Anton Kolesov <kolesov.anton.s@gmail.com>
2024-05-30 15:17:30 +0300

    Split some functions

--- CRS/server/src/router.c
+++ CRS/server/src/router.c
@@ -11,13 +11,16 @@ struct route {
 };
 
 // endpoints
-
-static void get_health(struct mg_connection *c, const struct mg_http_message *m) {
-    const char *json = "{\"message\":\"%m:%m\"}";
+static void ok_get_health_reply(struct mg_connection *c) {
+    const char *json = "{%m : %m}";
     mg_http_reply(c, 200, "", json, MG_ESC("status"), MG_ESC("OK"));
 }
 
-static void parse_attempt_tests(struct mg_str tests_json, struct CRS_test** tests, size_t*count_of_tests){
+static void get_health(struct mg_connection *c, const struct mg_http_message *m) {
+    ok_get_health_reply(c);
+}
+
+static void parse_attempt_tests(struct mg_str tests_json, struct CRS_test **tests, size_t *count_of_tests) {
     *count_of_tests = 0;
 
     size_t ofs = 0;
@@ -27,7 +30,7 @@ static void parse_attempt_tests(struct mg_str tests_json, struct CRS_test** test
         (*count_of_tests)++;
     }
 
-    struct CRS_test *tests_arr = malloc(sizeof (struct CRS_test) * *count_of_tests);
+    struct CRS_test *tests_arr = malloc(sizeof(struct CRS_test) * *count_of_tests);
 
     int i = 0;
     while ((ofs = mg_json_next(tests_json, ofs, &key, &val)) > 0) {
@@ -39,7 +42,7 @@ static void parse_attempt_tests(struct mg_str tests_json, struct CRS_test** test
     *tests = tests_arr;
 }
 
-static struct CRS_attempt parse_attempt(struct mg_str json){
+static struct CRS_attempt parse_attempt(struct mg_str json) {
     struct CRS_attempt attempt;
 
     char *code = mg_json_get_str(json, "$.code");
@@ -69,22 +72,35 @@ static struct CRS_attempt parse_attempt(struct mg_str json){
     return attempt;
 }
 
+static void fail_post_attempt_reply(struct mg_connection *c) {
+    const char *json = "{\"status\": %m}";
+    mg_http_reply(c, 200, "", json, MG_ESC("FAIL"));
+}
+
+static void pass_post_attempt_reply(struct mg_connection *c) {
+    const char *json = "{\"status\": %m}";
+    mg_http_reply(c, 200, "", json, MG_ESC("PASS"));
+}
+
+static void unknown_post_attempt_reply(struct mg_connection *c) {
+    const char *json = "{\"status\": %m}";
+    mg_http_reply(c, 200, "", json, MG_ESC("UNKNOWN"));
+}
+
 static void post_attempt(struct mg_connection *c, const struct mg_http_message *m) {
     struct CRS_attempt attempt = parse_attempt(m->body);
 
     enum CRS_run_status status = CRS_run_code(attempt);
 
-    const char *json = "{\"status\": %m}";
-
-    if(status == FAIL){
-        mg_http_reply(c, 200, "", json, MG_ESC("FAIL"));
+    if (status == FAIL) {
+        fail_post_attempt_reply(c);
     }
 
-    if(status == PASS){
-        mg_http_reply(c, 200, "", json, MG_ESC("PASS"));
+    if (status == PASS) {
+        pass_post_attempt_reply(c);
     }
 
-    mg_http_reply(c, 200, "", json, MG_ESC("UNKNOWN"));
+    unknown_post_attempt_reply(c);
 }
 
 #define ROUTES_COUNT 2
